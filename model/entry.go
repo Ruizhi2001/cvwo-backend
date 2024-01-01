d5e99dd78211b12d40eb6e4879258a678846599e
Ruizhi2001 <70838461+Ruizhi2001@users.noreply.github.com>
2024-01-01 18:39:16 +0800

    set up backend authentication and entry api

---
+++ model/entry.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"github.com/Ruizhi2001/cvwo-backend/database"
+	"gorm.io/gorm"
+)
+
+type Entry struct {
+	gorm.Model
+	Content string `gorm:"type:text" json:"content"`
+	UserID  uint
+}
+
+func (entry *Entry) Save() (*Entry, error) {
+	err := database.Database.Create(&entry).Error
+	if err != nil {
+		return &Entry{}, err
+	}
+	return entry, nil
+}
